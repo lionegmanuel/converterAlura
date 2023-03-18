5d568700a41e3234a8678c5f59c9d268b0b5b27c
MagnuZvn <lionegmanuel@gmail.com>
2023-03-17 21:29:51 -0300

    comienzo de interfaz grafica y proyecto alura

--- src/App.java
+++ src/App.java
@@ -1,232 +1,45 @@
 import java.util.Scanner;
 
+import javax.swing.Icon;
+import javax.swing.JOptionPane;
+
 public class App {
-  public static void main(String[] args) throws Exception {
-    // divisas
-    System.out.println("---CONVERSOR DE DIVISAS Y TEMPERATURA---");
-    System.out.println("---testeo de funcionamiento---\n");
-    currencyConverter currencyConverter = new currencyConverter();
-    currencyConverter.addCurrency(new currency("Peso Argentino"));
-    currencyConverter.addCurrency(new currency("Libra Exterlina"));
-    currencyConverter.addCurrency(new currency("USD"));
-    currencyConverter.addCurrency(new currency("EUR"));
-    currencyConverter.addCurrency(new currency("Yen Japones"));
-    currencyConverter.showCurrencies();
-    System.out.println("---");
-    // escalas de temperatura
+  public static void main(String[] args) {
 
-    temperatureConverter temperatureConverter = new temperatureConverter();
-    temperatureConverter.addTemperatureScale(new scale("Celsius"));
-    temperatureConverter.addTemperatureScale(new scale("Kelvin"));
-    temperatureConverter.addTemperatureScale(new scale("Fahrenheit"));
-    temperatureConverter.showTemperatureScale();
-    System.out.println("¿Que desea convertir?\n1-DIVISAS\n2-TEMPERATURAS");
-    Scanner userAnswer = new Scanner(System.in);
-    int answer = userAnswer.nextInt();
-    if (answer == 1)
-      System.out.println(
-          "Seleccione la divisa origen (Valor inicial):\n1-ARS\n2-USD\n3-EUR\n4-Libra Esterlina\n5-Yen Japones\n6-Won Surcoreano");
-    answer = userAnswer.nextInt();
-    Scanner userWorth = new Scanner(System.in); // valor a convertir
-    Scanner userAnswer2 = new Scanner(System.in); // divisa destino
-    if (answer == 1) { // ars
-      System.out.println(
-          "Seleccione la divisa destino (A la que desea realizar la conversion):\n1-USD\n2-EUR\n3-Libra Esterlina\n4-Yen Japones\n5-Won Surcoreano");
-      int answer2 = userAnswer2.nextInt();
-      if (answer2 == 1) { // usd
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth / 203;
-        System.out.println("RESULTADO:\n$ " + worth2);
+    String options = (JOptionPane.showInputDialog(null, "¿Que desea convertir?", "MENU", JOptionPane.QUESTION_MESSAGE,
+        null, new Object[] { "Divisas", "Temperaturas" }, "Selection")).toString();
+    switch (options) {
+      case "Divisas":
+        String currencyOption = (JOptionPane.showInputDialog(null, "Seleccione el metodo de cotizacion",
+            "DIVISAS A OPERAR", JOptionPane.QUESTION_MESSAGE, null, new Object[] { "PESO A DOLAR", "PESO A EURO",
+                "PESO A LIBRA ESTERLINA", "PESO A YEN JAPONES", "PESO A WON SURCOREANO" },
+            "convertSelection")).toString();
+        switch (currencyOption) {
+          case "PESO A DOLAR":
 
-      } else if (answer2 == 2) { // eur
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth / 215.4;
-        System.out.println("RESULTADO:\n€ " + worth2);
+          case "PESO A EURO":
+          case "PESO A LIBRA ESTERLINA":
+          case "PESO A YEN JAPONES":
+          case "PESO A WON SURCOREANO":
 
-      } else if (answer2 == 3) { // libra
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth / 245.72;
-        System.out.println("RESULTADO:\n₤ " + worth2);
-      } else if (answer2 == 4) { // yen
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth / 1.52;
-        System.out.println("RESULTADO:\n¥ " + worth2);
-      } else { // won
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth / 0.161;
-        System.out.println("RESULTADO:\n₩ " + worth2);
-      }
-    } else if (answer == 2) // usd
-    {
-      System.out.println(
-          "Seleccione la divisa destino (A la que desea realizar la conversion):\n1-USD\n2-EUR\n3-Libra Esterlina\n4-Yen Japones\n5-Won Surcoreano");
-      int answer2 = userAnswer2.nextInt();
-      if (answer2 == 1) { // ars
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 203;
-        System.out.println("RESULTADO:\n$ " + worth2);
-
-      } else if (answer2 == 2) { // eur
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 0.94;
-        System.out.println("RESULTADO:\n€ " + worth2);
-
-      } else if (answer2 == 3) { // libra
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 0.83;
-        System.out.println("RESULTADO:\n₤ " + worth2);
-      } else if (answer2 == 4) { // yen
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 133.6;
-        System.out.println("RESULTADO:\n¥ " + worth2);
-      } else { // won
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 1301;
-        System.out.println("RESULTADO:\n₩ " + worth2);
-      }
-    } else if (answer == 3) // eur
-    {
-      System.out.println(
-          "Seleccione la divisa destino (A la que desea realizar la conversion):\n1-USD\n2-EUR\n3-Libra Esterlina\n4-Yen Japones\n5-Won Surcoreano");
-      int answer2 = userAnswer2.nextInt();
-      if (answer2 == 1) { // usd
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 1.06;
-        System.out.println("RESULTADO:\n$ " + worth2);
-
-      } else if (answer2 == 2) { // ars
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 215.4;
-        System.out.println("RESULTADO:\n€ " + worth2);
-
-      } else if (answer2 == 3) { // libra
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 0.9;
-        System.out.println("RESULTADO:\n₤ " + worth2);
-      } else if (answer2 == 4) { // yen
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 141.7;
-        System.out.println("RESULTADO:\n¥ " + worth2);
-      } else { // won
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 1379.7;
-        System.out.println("RESULTADO:\n₩ " + worth2);
-      }
-    } else if (answer == 4) // libra
-    {
-      System.out.println(
-          "Seleccione la divisa destino (A la que desea realizar la conversion):\n1-USD\n2-EUR\n3-Libra Esterlina\n4-Yen Japones\n5-Won Surcoreano");
-      int answer2 = userAnswer2.nextInt();
-      if (answer2 == 1) { // usd
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 1.21;
-        System.out.println("RESULTADO:\n$ " + worth2);
-
-      } else if (answer2 == 2) { // eur
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 1.14;
-        System.out.println("RESULTADO:\n€ " + worth2);
-
-      } else if (answer2 == 3) { // ars
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 245.72;
-        System.out.println("RESULTADO:\n₤ " + worth2);
-      } else if (answer2 == 4) { // yen
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 161.7;
-        System.out.println("RESULTADO:\n¥ " + worth2);
-      } else { // won
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 1574.2;
-        System.out.println("RESULTADO:\n₩ " + worth2);
-      }
-    } else if (answer == 5) // yen
-    {
-      System.out.println(
-          "Seleccione la divisa destino (A la que desea realizar la conversion):\n1-USD\n2-EUR\n3-Libra Esterlina\n4-Yen Japones\n5-Won Surcoreano");
-      int answer2 = userAnswer2.nextInt();
-      if (answer2 == 1) { // usd
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth / 133.56;
-        System.out.println("RESULTADO:\n$ " + worth2);
-
-      } else if (answer2 == 2) { // eur
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth / 141.72;
-        System.out.println("RESULTADO:\n€ " + worth2);
-
-      } else if (answer2 == 3) { // libra
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth / 161.88;
-        System.out.println("RESULTADO:\n₤ " + worth2);
-      } else if (answer2 == 4) { // ars
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth / 245.7;
-        System.out.println("RESULTADO:\n¥ " + worth2);
-      } else { // won
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth * 6.41;
-        System.out.println("RESULTADO:\n₩ " + worth2);
-      }
-    } else // won
-    {
-      System.out.println(
-          "Seleccione la divisa destino (A la que desea realizar la conversion):\n1-USD\n2-EUR\n3-Libra Esterlina\n4-Yen Japones\n5-Won Surcoreano");
-      int answer2 = userAnswer2.nextInt();
-      if (answer2 == 1) { // usd
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth / 1301;
-        System.out.println("RESULTADO:\n$ " + worth2);
-
-      } else if (answer2 == 2) { // eur
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth / 1380.6;
-        System.out.println("RESULTADO:\n€ " + worth2);
-
-      } else if (answer2 == 3) { // libra
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth / 1575;
-        System.out.println("RESULTADO:\n₤ " + worth2);
-      } else if (answer2 == 4) { // yen
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth / 9.74;
-        System.out.println("RESULTADO:\n¥ " + worth2);
-      } else { // ars
-        System.out.println("Ingrese el valor a convertir:\n");
-        double worth = userWorth.nextDouble();
-        double worth2 = worth / 6.41;
-        System.out.println("RESULTADO:\n₩ " + worth2);
-      }
-
+        }
+      case "Temperaturas":
+        System.out.println("En desarrollo...");
     }
+
+  }
+
+  public static boolean inputValidator(String strInput) { // validador del ingreso
+
+    try {
+      double input = Double.parseDouble(strInput);
+      if (input >= 0 || input < 0)
+        ;
+      return true;
+    } catch (Exception e) {
+      System.out.println("¡ERROR! Ingrese caracteres validos");
+      return false;
+    }
+
   }
 }
--- src/currencyConverter.java
+++ src/currencyConverter.java
@@ -1,43 +1,36 @@
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 public class currencyConverter {
-  private ArrayList<currency> currencies; // registro del total de monedas disponibles para operar
-  private double changeType[][]; // registro del total de tipos de cambio entre monedas
-
-  public currencyConverter() {
-    this.currencies = new ArrayList<currency>();
-    this.changeType = new double[6][6]; // la dimension de la matriz DEBE SER proporcional a la cantidad de objetos //
-                                        // posibles a operar. En caso de que se deban agregar objetos, se debera
-                                        // actualizar la dimension de la matriz.
+  public void arsandusd(double worth) {
+    double argentineCotization = 201.3; // valor de 1 USD en pesos.
+    double result = worth / argentineCotization;
+    JOptionPane.showMessageDialog(null, "RESULTADO: $" + result);
   }
 
-  public void addCurrency(currency currency) { // registro de divisas a la lista
-    this.currencies.add(currency);
+  public void arsandeur(double worth) {
+    double argentineCotization = 216.4; // valor de 1 EUR en pesos.
+    double result = worth / argentineCotization;
+    JOptionPane.showMessageDialog(null, "RESULTADO: €" + result);
   }
 
-  public void showCurrencies() { // muestra de las divisas registradas
-    if (currencies.isEmpty())
-      System.out.println("¡ERROR!\nNo hay divisas registradas.");
-    else {
-      for (int x = 0; x < currencies.size(); x++) {
-        System.out.println(currencies.get(x));
-      }
-    }
-
+  public void arsandlib(double worth) {
+    double argentineCotization = 245.7; // valor de 1 libra en pesos.
+    double result = worth / argentineCotization;
+    JOptionPane.showMessageDialog(null, "RESULTADO: £" + result);
   }
 
-  public double[][] getChangeType() {
-    return changeType;
+  public void arsandyen(double worth) {
+    double argentineCotization = 1.53; // valor de 1 yen japones en pesos.
+    double result = worth / argentineCotization;
+    JOptionPane.showMessageDialog(null, "RESULTADO: ¥" + result);
   }
 
-  public ArrayList<currency> getCurrencies() {
-    return currencies;
-  }
-  // trabajo con la matriz y todos los posibles tipos de cambio
-
-  @Override
-  public String toString() {
-    return "texto de prueba";
+  public void arsandwon(double worth) {
+    double argentineCotization = 0.15; // valor de 1 won surcoreano en pesos.
+    double result = worth / argentineCotization;
+    JOptionPane.showMessageDialog(null, "RESULTADO: ₩" + result);
   }
 
 }
